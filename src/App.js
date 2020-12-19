44fe3bd476809866a76ff9dda2a35c0bf93b1b4c
vozsiberiana <70288823+vozsiberiana@users.noreply.github.com>
2020-12-19 19:31:20 +0100

    Added <a> props

--- src/App.js
+++ src/App.js
@@ -9,9 +9,9 @@ export default function App() {
 
      <Weather />
     <p>
-    This project was coded by <a href="https://www.linkedin.com/in/anna-knyazkina/" target="_blank">Anna Knyazkina</a> and it's{" "}  
+    This project was coded by <a href="https://www.linkedin.com/in/anna-knyazkina/" target="_blank" rel="noopener noreferrer">Anna Knyazkina</a> and it's{" "}  
      <a href="https://github.com/vozsiberiana/weather-react"
-     target="_blank">open-sourced</a> on GitHub
+     target="_blank" rel="noopener noreferrer">open-sourced</a> on GitHub
      </p>
      </div>
     </div>
