efcaff7f84b7e7dd72d8224fe14ebb1eeb5e21d4
vozsiberiana <70288823+vozsiberiana@users.noreply.github.com>
2020-12-19 11:24:17 +0100

    Added GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -5,7 +5,12 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-     Weather App
+     <h1>Weather App</h1>
+    <p>
+    This project was coded by <a href="https://www.linkedin.com/in/anna-knyazkina/" target="_blank">Anna Knyazkina</a> and it's{" "}  
+     <a href="https://github.com/vozsiberiana/weather-react"
+     target="_blank">open-sourced</a> on GitHub
+     </p>
     </div>
   );
 }
