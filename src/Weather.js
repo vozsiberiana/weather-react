24423f870fa1018a6c5f8cbd6e0dfe2584b039e3
vozsiberiana <70288823+vozsiberiana@users.noreply.github.com>
2020-12-25 12:48:46 +0100

    Icon chanded

--- src/Weather.js
+++ src/Weather.js
@@ -6,7 +6,7 @@ import "./Weather.css";
 export default function Weather(props) {
     
     const [weatherData, setWeatherData] = useState({ ready: false });
-
+const [city, setCity] = useState(props.defaultCity);
 
     function handleResponse(response) {
     console.log(response.data);
@@ -16,23 +16,39 @@ export default function Weather(props) {
         wind: response.data.wind.speed,
         city: response.data.name,
         description: response.data.weather[0].description,
-        iconUrl: "https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png",
+        iconUrl: `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
         humidity: response.data.main.humidity,
         date: new Date(response.data.dt * 1000)
     });
 
 }
 
+function handleSubmit(event) {
+    event.preventDefault();
+    search();
+}
+
+function search() {
+    const apiKey = "0b1fa8952349f479be72640e8d64dd95";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+}
+
+function handleCityChange(event) {
+setCity(event.target.value);
+}
+
 if (weatherData.ready) {
 return (
     <div className="Weather">
-    <form>
+    <form onSubmit={handleSubmit}>
     <div className="row">
     <div className="col-9">
         <input type="search" 
             placeholder="Enter a city.."
             className="form-control"
             autoFocus="on"
+            onChange={handleCityChange}
         />
         </div>
         <div className="col-3">
@@ -65,9 +81,7 @@ return (
     </div>
     );
 } else {
-    const apiKey = "0b1fa8952349f479be72640e8d64dd95";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
-    axios.get(apiUrl).then(handleResponse);
+search();
     return "loading..";
 }
 
